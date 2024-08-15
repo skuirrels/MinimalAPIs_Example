26dd880f034cefe9276a52e8d70612c88a6ee18e
Skuirrels <ty.omidi@outlook.com>
2024-08-15 19:27:23 +0100

    Split out Order and Order Respository

--- MinimalAPIs_Example/Repositories/Orders.cs
+++ MinimalAPIs_Example/Repositories/Orders.cs
@@ -111,35 +111,3 @@ public class Orders
     }
 }
 
-public class Order
-{
-    public Guid Id { get; set; } = Guid.NewGuid();
-    public int OrderNumber { get; set; }
-    public string? CustomerName { get; set; }
-    public string? ProductName { get; set; }
-    public OrderState State { get; set; } = OrderState.New;
-    public string StateName => State.ToString();
-
-    public DateTime CreatedDateTime { get; private set; } = DateTime.UtcNow;
-    public DateTime? UpdatedDateTime { get; private set; } = DateTime.UtcNow;
-    public int TotalQuantity => OrderLines.Sum(orderLine => orderLine.Quantity);
-
-    public List<CustomField> CustomFields { get; set; } = [];
-    public List<OrderLine> OrderLines { get; set; } = [];
-}
-
-public class OrderLine
-{
-    public Guid Id { get; set; } = Guid.NewGuid();
-    public int LineNumber { get; set; }
-    public string? Product { get; set; }
-    public int Quantity { get; set; }
-}
-
-public enum OrderState
-{
-    New = 1,
-    Processing = 2,
-    Shipped = 3,
-    Delivered = 4
-}
